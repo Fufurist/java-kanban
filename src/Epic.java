ef862b99e1c208134d826944fd50148063fab2a0
Fufurist <hopilki.popito@gmail.com>
2025-05-04 22:21:46 +0300

    Created Epic.java and SubTask.java drafts

---
+++ src/Epic.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Epic extends Task{
+    private final ArrayList<Integer> subTasksIds;
+
+    public Epic(String name, String description, TaskStatus status) {
+        super(name, description, status);
+        this.subTasksIds = new ArrayList<>();
+        //Новый эпик всегда создаётся первым, а потом к нему цепляются подзадачи
+    }
+
+    public Epic(int id, String name, String description, TaskStatus status, ArrayList<Integer> subclassesIds) {
+        super(id, name, description, status);
+        this.subTasksIds = subclassesIds;
+        //Создание нового эпика на замену существующему
+    }
+
+    public void addSubTask(int subTaskId){
+        subTasksIds.add(subTaskId);
+    }
+
+    public ArrayList<Integer> getSubTasksIds(){
+        return subTasksIds;
+    }
+}
---
+++ src/SubTask.java
@@ -0,0 +1,18 @@
+public class SubTask extends Task{
+    private final int epicId;
+
+    public SubTask(String name, String description, TaskStatus status, int epicId) {
+        super(name, description, status);
+        this.epicId = epicId;
+        //При создании подзадачи уже известен её эпик
+    }
+
+    public SubTask(int id, String name, String description, TaskStatus status, int epicId) {
+        super(id, name, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId(){
+        return epicId;
+    }
+}
