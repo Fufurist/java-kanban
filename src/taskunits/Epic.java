cad5a6a31062ee9b0a2b8bc92835624398553d47
Fufurist <hopilki.popito@gmail.com>
2025-07-12 10:15:27 +0300

    Feature: Adjust SubTask and Epic to new Task fields

--- src/taskunits/Epic.java
+++ src/taskunits/Epic.java
@@ -1,13 +1,15 @@
 package taskunits;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Epic extends Task {
     private final ArrayList<Integer> subTasksIds;
 
     //Поскольку теперь менеджер сам меняет поля, пользователям не нужно самим указывать список подзадач
-    public Epic(String name, String description) {
-        super(name, description, TaskStatus.NEW);//пусть по умолчанию новый будет
+    public Epic(String name, String description, LocalDateTime startTime, Duration duration) {
+        super(name, description, TaskStatus.NEW, startTime, duration);//пусть по умолчанию новый будет
         this.subTasksIds = new ArrayList<>();
     }
 
@@ -29,13 +31,14 @@ public class Epic extends Task {
 
     @Override
     public String toString() {
-        return String.format("%d,EPIC,%s,%s,%s", getId(), getName(), getStatus().name(),
-                getDescription());
+        return String.format("%d,Epic,%s,%s,%s,%s,%d", getId(), getName(), getStatus().name(),
+                getDescription(), getStartTime().toString(), getDuration().toMinutes());
     }
 
     public static Epic toEpic(String line) {
         String[] parameters = line.split(",");
-        Epic result = new Epic(parameters[2], parameters[4]);
+        Epic result = new Epic(parameters[2], parameters[4], LocalDateTime.parse(parameters[5]),
+                Duration.ofMinutes(Long.parseLong(parameters[6])));
         result.setId(Integer.parseInt(parameters[0]));
         switch (parameters[3]) {
             case "NEW":
--- src/taskunits/SubTask.java
+++ src/taskunits/SubTask.java
@@ -1,11 +1,16 @@
 package taskunits;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Duration;
+
 public class SubTask extends Task {
     private final int epicId;
 
     //Также единственный конструктор. При перезаписи поле принадлежности к эпику все равно будет игнорироваться
-    public SubTask(String name, String description, TaskStatus status, int epicId) {
-        super(name, description, status);
+    public SubTask(String name, String description, TaskStatus status,
+                   LocalDateTime startTime, Duration duration, int epicId) {
+        super(name, description, status, startTime, duration);
         this.epicId = epicId;
     }
 
@@ -15,13 +20,14 @@ public class SubTask extends Task {
 
     @Override
     public String toString() {
-        return String.format("%d,SUBTASK,%s,%s,%s,%d", getId(), getName(), getStatus().name(),
-                getDescription(), getEpicId());
+        return String.format("%d,SUBTASK,%s,%s,%s,%s,%d,%d", getId(), getName(), getStatus().name(),
+                getDescription(), getStartTime().toString(), getDuration().toMinutes(), getEpicId());
     }
 
     public static SubTask toSubTask(String line) {
         String[] parameters = line.split(",");
-        SubTask result = new SubTask(parameters[2], parameters[4], TaskStatus.NEW, Integer.parseInt(parameters[5]));
+        SubTask result = new SubTask(parameters[2], parameters[4], TaskStatus.NEW, LocalDateTime.parse(parameters[5]),
+                Duration.ofMinutes(Long.parseLong(parameters[6])), Integer.parseInt(parameters[7]));
         result.setId(Integer.parseInt(parameters[0]));
         switch (parameters[3]) {
             case "NEW":
